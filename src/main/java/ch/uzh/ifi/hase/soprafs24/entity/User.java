5b2471310b5fa7417b6388cee1f4719035a1da99
steinerchristof <126617581+steinerchristof@users.noreply.github.com>
2024-04-09 18:52:38 +0200

    User Entity and Service #99 #102

--- src/main/java/ch/uzh/ifi/hase/soprafs24/entity/User.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/entity/User.java
@@ -9,7 +9,7 @@ import java.io.Serializable;
  * Internal User Representation
  * This class composes the internal representation of the user and defines how
  * the user is stored in the database.
- * Every variable will be mapped into a database field with the @Column
+ * Every variable will  be mapped into a database field with the @Column
  * annotation
  * - nullable = false -> this cannot be left empty
  * - unique = true -> this value must be unqiue across the database -> composes
