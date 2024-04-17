033ca5c073cceb68305fb5b966333773aa987bb4
gian.seifert <gian.seifert@uzh.ch>
2024-04-17 10:49:26 +0200

    #79 lobbyservice tests for deletion and joining

--- src/test/java/ch/uzh/ifi/hase/soprafs24/service/LobbyServiceTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs24/service/LobbyServiceTest.java
@@ -96,4 +96,73 @@ public class LobbyServiceTest {
 
         verify(lobbyRepository, times(1)).delete(testLobby);
     }
+
+    @Test
+    public void deleteLobby_nonExistingLobby_throwsException() {
+        Long nonExistingLobbyId = 2L;
+        when(lobbyRepository.findById(nonExistingLobbyId)).thenReturn(Optional.empty());
+
+        assertThrows(ResponseStatusException.class, () -> lobbyService.deleteLobby(nonExistingLobbyId, testLobby.getLobbyOwner()));
+    }
+
+    @Test
+    public void deleteLobby_nonOwner_throwsException() {
+        Long nonOwner = 2L;
+        when(lobbyRepository.findById(testLobby.getLobbyId())).thenReturn(Optional.of(testLobby));
+
+        assertThrows(ResponseStatusException.class, () -> lobbyService.deleteLobby(testLobby.getLobbyId(), nonOwner
+        ));
+    }
+
+    @Test
+    public void deleteLobby_nonExistingUser_throwsException() {
+        Long nonExistingUser = 2L;
+        when(lobbyRepository.findById(testLobby.getLobbyId())).thenReturn(Optional.of(testLobby));
+        when(userRepository.existsById(nonExistingUser)).thenReturn(false);
+
+        assertThrows(ResponseStatusException.class, () -> lobbyService.deleteLobby(testLobby.getLobbyId(), nonExistingUser));
+    }
+
+    @Test
+    public void joinLobby_existingLobbyAndUser_joinsLobby() {
+        Long userId = 2L;
+        when(lobbyRepository.findByLobbyJoinCode(testLobby.getLobbyJoinCode())).thenReturn(Optional.of(testLobby));
+
+        lobbyService.joinLobby(userId, testLobby);
+
+        assertEquals(testLobby.getPlayers().contains(userId), true);
+    }
+
+    @Test
+    public void joinLobby_fullLobby_throwsException() {
+        Long userId = 2L;
+        testLobby.addPlayer(2L);
+        testLobby.addPlayer(3L);
+        testLobby.addPlayer(4L);
+        testLobby.addPlayer(5L);
+        testLobby.addPlayer(6L);
+        testLobby.addPlayer(7L);
+        testLobby.addPlayer(8L);
+        testLobby.addPlayer(9L);
+        when(lobbyRepository.findByLobbyJoinCode(testLobby.getLobbyJoinCode())).thenReturn(Optional.of(testLobby));
+
+        assertThrows(ResponseStatusException.class, () -> lobbyService.joinLobby(userId, testLobby));
+    }
+
+    @Test
+    public void joinLobby_nonExistingUser_throwsException() {
+        Long nonExistingUser = 200L;
+        when(lobbyRepository.findByLobbyJoinCode(testLobby.getLobbyJoinCode())).thenReturn(Optional.of(testLobby));
+        when(userRepository.existsById(nonExistingUser)).thenReturn(false);
+
+        assertThrows(ResponseStatusException.class, () -> lobbyService.joinLobby(nonExistingUser, testLobby));
+    }
+
+    @Test
+    public void joinLobby_nonExistingLobby_throwsException() {
+        Long userId = 2L;
+        when(lobbyRepository.findByLobbyJoinCode(testLobby.getLobbyJoinCode())).thenReturn(Optional.empty());
+
+        assertThrows(ResponseStatusException.class, () -> lobbyService.joinLobby(userId, testLobby));
+    }
 }
