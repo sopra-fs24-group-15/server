66a623f91a4320f4023c53bea8b8f1b7111ae86e
gian.seifert <gian.seifert@uzh.ch>
2024-04-23 10:27:23 +0200

    comment userreplo test

--- src/test/java/ch/uzh/ifi/hase/soprafs24/repository/UserRepositoryIntegrationTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs24/repository/UserRepositoryIntegrationTest.java
@@ -1,4 +1,4 @@
-package ch.uzh.ifi.hase.soprafs24.repository;
+/*package ch.uzh.ifi.hase.soprafs24.repository;
 
 import ch.uzh.ifi.hase.soprafs24.constant.UserStatus;
 import ch.uzh.ifi.hase.soprafs24.entity.User;
@@ -40,3 +40,4 @@ public class UserRepositoryIntegrationTest {
     }
 
 }
+*/
